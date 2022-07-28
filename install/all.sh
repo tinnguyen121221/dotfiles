9ba66b3fa835e8e8f8230955b67a82ff28662507
tinnguyentg <tinnguyen121221@gmail.com>
2022-07-28 22:32:52 +0700

    update install all.sh

--- install/all.sh
+++ install/all.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-backup_and_link() {
-
-    if [ -f $2 ] || [ -d $2 ]; then
-        mv -f $2 ~/.backup
-    fi
-
-    ln -svf $1 $2
-}
 
 ./install/commons.sh
 ./install/ohmyzsh.sh
@@ -26,10 +18,3 @@ backup_and_link() {
 ./install/psql.sh
 
 
-mkdir -p ~/.backup
-mkdir -p ~/.config/nvim
-backup_and_link "${PWD}/.zshrc" ~/.zshrc
-backup_and_link "${PWD}/.zsh_aliases" ~/.zsh_aliases
-backup_and_link "${PWD}/.vimrc" ~/.config/nvim/init.vim
-backup_and_link "${PWD}/.mytheme.omp.json" ~/.mytheme.omp.json
-backup_and_link "${PWD}/.zfunc/" ~/.zfunc
