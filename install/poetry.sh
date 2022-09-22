eddaa17360e308d474bcfa19010d2ace0d147b94
tinnguyentg <tinnguyen121221@gmail.com>
2022-09-22 22:43:06 +0700

    Updated poetry install script

--- install/poetry.sh
+++ install/poetry.sh
@@ -4,6 +4,6 @@
 if [[ -x "$(command -v poetry)" ]]; then
 				poetry self update
 else
-        curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+        curl -sSL https://install.python-poetry.org | python3 -
 
 fi
